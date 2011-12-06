be5b444a0bd356a73bcce67f046e3b6d31e01c9a
IceManX93X <sj_93@web.de>
2011-12-06 22:12:49 +0100

    Kobborg works :)

--- src/server/scripts/Custom/boss_kobborg.cpp
+++ src/server/scripts/Custom/boss_kobborg.cpp
@@ -1,80 +1,97 @@
 #include "ScriptPCH.h"
 
-enum eEnums
+enum Spells
 {
-    SAY_AGGRO               	= -1542008,
-
     SPELL_CLEAVE       			= 86745,
-    SPELL_POISON_CLOUD      	= 30916,
-  //SPELL_SUICIDE       		= 7,
-	SPELL_ASCEND_TO_THE_HEAVENS = 64487,		//Enrage
-    SPELL_POISON           		= 30914
+	SPELL_DEVOTION_AURA       	= 58944,
+	SPELL_EARTH_SHIELD			= 379,		
+    SPELL_BERSERK				= 28498
 };
 
-class boss_broggok : public CreatureScript
+enum Yells
 {
-    public : boss_kobborg() : CreatureScript("boss_kobborg") { }
-		
-		creatureAI*getAI(Creature* creature) const
+	SAY_AGGRO                   = -1542008
+};
+
+enum Events
+{
+
+};
+
+class boss_kobborg : public CreatureScript
+{
+    public : 
+			boss_kobborg() : CreatureScript("boss_kobborg") { }
+			
+		CreatureAI* GetAI(Creature* creature) const
 		{
 			return new boss_kobborgAI (creature);
 		}
 		
 		struct boss_kobborgAI : public ScriptedAI
         {
+
+			boss_kobborgAI(Creature* c) : ScriptedAI(c)
+            {
+				pInstance = c->GetInstanceScript();
+            }
+
             uint32 Cleave_Timer;
-            uint32 PoisonSpawn_Timer;
-        //  uint32 Suicide_Timer;
-			uint32 AscendToTheHeavens_Timer;
+            uint32 DevotionAura_Timer;
+            uint32 EarthShield_Timer;
+			uint32 Berserk_Timer;
+			
+			InstanceScript* pInstance;
 
             void Reset()
             {
-                Cleave_Timer = 5000;
-                PoisonSpawn_Timer = 8000;
-                AscendToTheHeavens_Timer = 40000;
-            }
+				Cleave_Timer = 5000;
+                DevotionAura_Timer = 1;
+				EarthShield_Timer = 1;
+                Berserk_Timer = 1;
+	        }
 
             void EnterCombat(Unit* /*who*/)
             {
-                
+				               
             }
 
             void UpdateAI(const uint32 diff)
             {
                 if (!UpdateVictim())
                     return;
-
-                if (Cleave_Timer <= diff)
-                {
-                    DoCast(me->getVictim(), SPELL_CLEAVE);
-                    Cleave_Timer = 4000+rand()%8000;
-                }
-                else
-                    Cleave_Timer -=diff;
-
-                if (PoisonSpawn_Timer <= diff)
-                {
-                    DoCast(me, SPELL_POISON_CLOUD);
-                    PoisonSpawn_Timer = 20000;
-                }
-                else
-                    PoisonSpawn_Timer -=diff;
 				
-				if (AscendToTheHeavens_Timer <= diff)
-				{
-					DoCast(me, SPELL_ASCEND_TO_THE_HEAVENS);
-					AscendToTheHeavens_Timer = 0;
-				}
-				else AscendToTheHeavens_Timer -=diff;
+				if (Cleave_Timer <= diff)
+                {
+					DoCast(me->getVictim(), SPELL_CLEAVE);
+                    Cleave_Timer = 5000;
+                } else Cleave_Timer -= diff;
+
+				if (DevotionAura_Timer <= diff)                                              
+                {
+					DoCast(me, SPELL_DEVOTION_AURA);
+					DevotionAura_Timer = 1;
+                } else DevotionAura_Timer -= diff;
+				
+                if (EarthShield_Timer)
+                {
+					DoCast(me, SPELL_EARTH_SHIELD);
+					EarthShield_Timer = 1;
+                } else EarthShield_Timer -= diff;
+
+				if (Berserk_Timer <= diff)                                       
+                {
+					DoCast(me, SPELL_BERSERK);
+                    Berserk_Timer = 1;
+                } else Berserk_Timer -= diff;
 
                 DoMeleeAttackIfReady();
             }
-
-            void JustDied(Unit* /*who*/)
-            {
-				
-            }
-
+			
+			void JustDied(Unit* /*Victim*/)
+			{
+			
+			}
         };
 };
 
